b927c069e3026ed8207869ec6ac314febd07b552
c0mg3k <c0mg3k@outlook.com>
2020-04-04 21:14:32 -0500

    initialization

---
+++ ChampSmash/Models/DataPopulation/Champion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChampSmash.Models.DataPopulation
+{
+    public class Champion
+    {
+    }
+}
---
+++ ChampSmash/Models/User/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChampSmash.Models.User
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
